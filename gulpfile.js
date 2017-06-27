ffb69997a022578d317c016725b79133845a37e3
DavidFilth <davidfilth.197694@gmail.com>
2017-06-27 14:54:18 -0700

    Add return statements to each task

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,17 @@ var ts = require('gulp-typescript');
 var nodemon = require('gulp-nodemon');
 
 gulp.task('build', ()=>{
-    gulp.src('server/src/**/*.ts')
+    return gulp.src('server/src/**/*.ts')
     .pipe(ts())
     .pipe(gulp.dest('server/build/'));
 })
-gulp.task('watch', ()=>{
-    gulp.watch('server/src/**/*.ts', ['build']);
+gulp.task('watch', ['build'], ()=>{
+    return gulp.watch('server/src/**/*.ts', ['build']);
+});
+gulp.task('server', ['build', 'watch'], function(){
+    return nodemon({
+            script: 'server/build/server.js',
+            env: { 'NODE_ENV': 'development' }
+        });
 })
-gulp.task('start', function () {
-  nodemon({
-    script: 'server/build/server.js',
-    env: { 'NODE_ENV': 'development' }
-  })
-})
-gulp.task('default', ['build', 'watch', 'start']);
+gulp.task('default', ['build', 'watch', 'server']);
