be31cc4da350ffadb1e8404e4490c80e121b5528
DavidFilth <davidfilth.197694@gmail.com>
2017-07-03 10:24:29 -0700

    Create a single service for the management of the user's account

--- client/src/app/services/users.service.ts
+++ client/src/app/services/users.service.ts
@@ -3,14 +3,21 @@ import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class SignupService {
+export class UsersService {
 
-  constructor(private http: Http) {
-  }
+  constructor(private http: Http) {}
+
   signUpUser(user){
     var headers = new Headers(); 
     headers.append('Content-Type', 'application/json');
     return this.http.post('/api/register', JSON.stringify(user), {headers: headers})
       .map( res => res.json());
   }
+  logIn(user: any){
+    var headers = new Headers(); 
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('/api/login', JSON.stringify(user), {headers: headers})
+      .map( res => console.log(res));
+  }
+
 }
