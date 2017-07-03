7eeeda423139dbc312d6e518aea2f8a79d4d341e
DavidFilth <davidfilth.197694@gmail.com>
2017-07-03 11:08:17 -0700

    Add the angular routing component for the SPA

---
+++ client/src/app/routes/routing.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "../components/login/login.component";
+import { SignupComponent } from "../components/signup/signup.component";
+import { HomeComponent } from '../components/home/home.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'dashboard', component: DashboardComponent}
+];
+export const routing = RouterModule.forRoot(routes);
