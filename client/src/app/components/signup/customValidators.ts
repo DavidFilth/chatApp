7f644bedbe04226fee0abcbb6ae7cec605379db0
DavidFilth <davidfilth.197694@gmail.com>
2017-07-09 21:51:26 -0700

    Inject the custom validators as a service

--- client/src/app/components/signup/customValidators.ts
+++
@@ -1,7 +0,0 @@
-import { FormGroup } from '@angular/forms'
-function equalValidator({ value }: FormGroup): { [key: string]: any } {
-  const [first, ...rest] = Object.keys(value || {});
-  const valid = rest.every(v => value[v] === value[first]);
-  return valid ? null : { equal: true };
-}
-export {equalValidator}
