af698cd5fcce3d9c505919c837d6678d2d8f10a5
DavidFilth <davidfilth.197694@gmail.com>
2017-07-09 18:23:24 -0700

    Change the fields of the login form

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router'
 
 import { UsersService } from '../../services/users.service';
+import { MessagesService } from '../../services/messages.service';
 
 import 'rxjs/add/operator/catch';
 
@@ -13,23 +14,43 @@ import 'rxjs/add/operator/catch';
 })
 
 export class LoginComponent {
-  loginForm: FormGroup;
+  formModel: FormGroup;
   messageError: string;
   constructor(fb: FormBuilder, 
               private usersService: UsersService,
-              private router: Router){
-    this.loginForm = fb.group({
-      'username': ['', [Validators.required, Validators.minLength(6)]],
+              private router: Router,
+              private messages: MessagesService){
+    this.formModel = fb.group({
+      'email': ['', Validators.email ],
       'password': ['', [Validators.required, Validators.minLength(6)]]
     });
   }
   logUser(){
-    let {username, password} = this.loginForm.value;
-    this.usersService.logIn({username, password})
+    let {email, password} = this.formModel.value;
+    this.usersService.logIn({email, password})
       .catch((error, caught) =>{
+        this.messages.emit({ content: "There's something wrong with the username or the password, please try again", type: 'alert-danger'})
         return [];
       }).subscribe(data =>{
         this.router.navigateByUrl('/dashboard');
+        this.messages.emit({ content: 'You are now logged in', type: 'alert-info'})
       });
+  }
+  hasError(field, valid, optional){
+    let control: any = this.formModel.get(field);
+    if(control instanceof FormGroup){
+      if(valid === undefined && optional === undefined){
+        return !(control.errors === null) && !control.pristine;
+      }
+      control = control.controls[valid];
+      valid = optional;
+    }
+    if(control.errors === null){
+      return false;
+    }
+    if( valid === undefined){
+      return true && !control.pristine;
+    }
+    return control.errors[valid] && !control.pristine;
   }
 }
