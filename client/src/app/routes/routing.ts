4e8cff159d5652e87ff4566529abc0f04ff41954
DavidFilth <davidfilth.197694@gmail.com>
2017-07-09 21:54:05 -0700

    Added child navigation to the dashboard component

--- client/src/app/routes/routing.ts
+++ client/src/app/routes/routing.ts
@@ -1,14 +1,21 @@
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from "../components/login/login.component";
 import { SignupComponent } from "../components/signup/signup.component";
 import { HomeComponent } from '../components/home/home.component';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import {ContactListComponent} from '../components/contact-list/contact-list.component'
+import { AddContactComponent } from '../components/add-contact/add-contact.component'
+
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'signup', component: SignupComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
+    {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], children: [
+        {path: '', component: ContactListComponent },
+        {path: 'addcontact', component: AddContactComponent}
+    ]}
 ];
 export const routing = RouterModule.forRoot(routes);
