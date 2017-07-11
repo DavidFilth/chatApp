72952c22083bc49afe89bae1289bb6e948b72e6e
DavidFilth <davidfilth.197694@gmail.com>
2017-07-11 09:49:52 -0700

    Add extra validator to look for an email

--- client/src/app/services/validators.service.ts
+++ client/src/app/services/validators.service.ts
@@ -11,6 +11,7 @@ export class ValidatorsService {
 
   constructor(private http: Http) {
     this.availableEmail = this.availableEmail.bind(this);
+    this.existingEmail = this.existingEmail.bind(this);
    }
   equalValidator({ value }: FormGroup): { [key: string]: any } {
     const [first, ...rest] = Object.keys(value || {});
@@ -21,6 +22,12 @@ export class ValidatorsService {
     return this.http.get('api/availableEmail/' + control.value).map(function(data : any){
       let res = data.json()
       return res.available ? null : {notAvailable : true };
+    }).debounceTime(500);
+  }
+  existingEmail(control: FormControl) : Observable<any>{
+    return this.http.get('api/availableEmail/' + control.value).map(function(data){
+      let res = data.json();
+      return res.available ? {existEmail : true } : null;
     }).debounceTime(500);
   }
 }
