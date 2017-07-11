15e34f3c6af3b18a0a37834b0839383b50e55522
DavidFilth <davidfilth.197694@gmail.com>
2017-07-11 09:56:01 -0700

    Add functionality in the server to handle the friendship requests

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -75,6 +75,64 @@ router.get('/availableEmail/:email', function (req, res) {
     res.send({available: data === null});
   });
 });
+router.post('/user/sendFriendshipRequest', function(req, res){
+  User.findOne({email: req.body.contact},function(err, contact :any){
+    if(err){
+      res.send(err)
+    }
+    User.findById(req.body.userId, function(error, data : any){
+      if(error){
+        res.send(error);
+      }
+      else{
+        if(data.pendingRequests.indexOf(contact._id) !== -1){
+          res.send({status: 3, name: contact.name})
+        }
+      //check if there's already  on the contacts list
+      else if(contact.contacts.indexOf(req.body.userId) !== -1){
+        res.send({status: 1, name: contact.name});
+      }
+      //check if there's already a pending friend request
+      else if(contact.pendingRequests.indexOf(req.body.userId) !== -1 ){
+        res.send({status: 2, name: contact.name});
+      }
+      else{
+        contact.pendingRequests.push(req.body.userId);
+        contact.save();
+        res.send({status: 0, name: contact.name});
+      }
+      }
+    });
+  });
+});
+router.post('/user/resolveFriendRequest', function(req, res){
+  User.findById(req.body.userId, function(err, user : any){
+    if(err){
+      res.send(err);
+    } else if(user){
+      let index = user.pendingRequests.indexOf(req.body.contactId);
+      user.pendingRequests.splice(index, 1);
+      user.save();
+      if(!req.body.response){
+        res.send({status: false});
+      }
+      else{
+        user.contacts.push(req.body.contactId);
+        user.save();
+        User.findById(req.body.contactId, function(err2, contact: any){
+
+          if(err2){
+            res.send(err)
+          } else{
+            contact.contacts.push(req.body.userId);
+            contact.save();
+            res.send({status: true});
+          }
+        });
+      }
+    }
+  })
+});
 
 router.use('/search', search);
 
