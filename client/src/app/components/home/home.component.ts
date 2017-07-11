3ac90a4c2e3f5152249ebd9d1af8a72893f14707
DavidFilth <davidfilth.197694@gmail.com>
2017-07-11 13:58:58 -0700

    Remove the Users service from the component

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 
 @Component({
@@ -6,11 +6,7 @@ import { UsersService } from '../../services/users.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor(private usersServ: UsersService) {}
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
