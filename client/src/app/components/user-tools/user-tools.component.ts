cbcc7c8f53a7a7362fff91943a80c507998ce2db
DavidFilth <davidfilth.197694@gmail.com>
2017-07-11 09:45:22 -0700

    Inject the user service to the component

--- client/src/app/components/user-tools/user-tools.component.ts
+++ client/src/app/components/user-tools/user-tools.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { UsersService } from '../../services/users.service'
 
 @Component({
   selector: 'app-user-tools',
   templateUrl: './user-tools.component.html',
   styleUrls: ['./user-tools.component.css']
 })
-export class UserToolsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class UserToolsComponent {
+  user: {};
+  constructor(private userServ: UsersService) {
+    this.user = userServ.getUser();
   }
 
 }
