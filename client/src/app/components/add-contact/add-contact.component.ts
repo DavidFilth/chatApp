7ff332ede3a8834debbd1e568e9b4754ab4a2de7
DavidFilth <davidfilth.197694@gmail.com>
2017-07-11 09:47:44 -0700

    Add validators to check the email field of the form element

--- client/src/app/components/add-contact/add-contact.component.ts
+++ client/src/app/components/add-contact/add-contact.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { UsersService } from '../../services/users.service'
+import { ValidatorsService } from '../../services/validators.service'
+import { MessagesService } from '../../services/messages.service'
 
 @Component({
   selector: 'app-add-contact',
@@ -8,13 +11,51 @@ import { UsersService } from '../../services/users.service'
   styleUrls: ['./add-contact.component.css']
 })
 export class AddContactComponent{
-  constructor(private usersServ: UsersService) {}
-  addFriend(data){
-    let context = this;
-    this.usersServ.searchByEmail(data.email).subscribe(data =>{
-      context.usersServ.addContact(data.id).subscribe(function(d){
-        console.log('data recibed =', d);
-      })
+  formModel: FormGroup;
+  constructor(private usersServ: UsersService, private validators: ValidatorsService, private fb: FormBuilder, private message: MessagesService) {
+    this.formModel = fb.group({
+      'email': ['', Validators.email, validators.existingEmail]
     })
+  }
+  sendFriendRequest(){
+    let context = this;
+    let input = this.formModel.value;
+    let user = this.usersServ.getUser();
+    if(input.email === user.email ){
+      this.message.emit({content:"You can't send a friend request to yourself", type: "alert-warning" });
+    } else{
+      this.usersServ.friendshipRequest({userId: user._id, contact: input.email})
+        .subscribe(function(data: any){
+          if(data.status === 0) {
+            context.message.emit({content:`Your request has been sent to ${data.name}`, type: "alert-info" });
+          }
+          if(data.status === 1) {
+            context.message.emit({content:`${data.name} is already in your contact list`, type: "alert-warning" });
+          }
+          if(data.status === 2) {
+            context.message.emit({content:`You have already send a request to ${data.name}`, type: "alert-warning" });
+          }
+          if(data.status === 3){
+            context.message.emit({content:`You have a pending friend request from ${data.name}`, type: "alert-warning" });
+          }
+        });
+    }
+  }
+  hasError(field, valid, optional){
+    let control: any = this.formModel.get(field);
+    if(control instanceof FormGroup){
+      if(valid === undefined && optional === undefined){
+        return !(control.errors === null) && !control.pristine;
+      }
+      control = control.controls[valid];
+      valid = optional;
+    }
+    if(control.errors === null){
+      return false;
+    }
+    if( valid === undefined){
+      return true && !control.pristine;
+    }
+    return control.errors[valid] && !control.pristine;
   }
 }
